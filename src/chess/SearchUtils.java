2b3233de12a196fe52c1e8b834a4dcd9247b11a3
evandam <ecv92@yahoo.com>
2013-12-02 00:18:21 -0500

    implemented AB search...haven't tested

--- src/chess/SearchUtils.java
+++ src/chess/SearchUtils.java
@@ -1,16 +1,19 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.Map;
 
 public class SearchUtils {
 	// use this for in the cutoff test (limit 5 seconds per search?)
 	public static long startSearchTime = 0;
 	
 	// have cutoff here which would be number of ply's?
+	private static int maxPly = 5;
 	
 	public static void AlphaBetaSearch(ChessBoard board) {
 		startSearchTime = new Date().getTime();
-		int v = MaxValue(board, Integer.MIN_VALUE, Integer.MAX_VALUE);
+		double v = MaxValue(board, Integer.MIN_VALUE, Integer.MAX_VALUE, 0);
 		
 		// return move
 	}
@@ -22,47 +25,78 @@ public class SearchUtils {
 		else
 			return board.terminalTest();
 	}
-	
-	public static int MaxValue(ChessBoard board, int alpha, int beta) {
+
+	public static double MaxValue(ChessBoard board, double alpha, double beta, int currentPly) {
 		// if terminal / cutoff reached
-		// return board.Utility();
 		if(cutoffTest(board))
 			return board.Utility();
 		
-		int v = Integer.MIN_VALUE;
+		currentPly++;
+		
+		double v = Double.MIN_VALUE;
 		
 		// for each move we can make in this state
-		//	v = Max(v, MinValue(/*result of this move*/, alpha, beta)); 
+		//	v = Max(v, MinValue(/*result of this move*/, alpha, beta, currentPly)); 
 		//	if(v > beta)
 		//		return v;
 		//	alpha = Max(alpha, v);
 			
+		Map<Integer, ArrayList<byte[]>> moves = board.getAllLegalMoves(board.getOurColor());
+		ChessBoard newBoard;
+		for(Integer startPos : moves.keySet()) {
+			byte startFile = (byte) (startPos % 10);
+			byte startRank = (byte) (startPos - startFile % 100);
+			for(byte[] endPos : moves.get(startPos)) {
+				newBoard = board.clone();
+				newBoard.move(startRank, startFile, endPos[0], endPos[1]);
+				v = Math.max(v, MinValue(newBoard, alpha, beta, currentPly));
+				if(v >= beta)
+					return v;
+				alpha = Math.max(alpha, v);
+			}
+		}
 		return v;
 	}
 	
-	public static int MinValue(ChessBoard board, int alpha, int beta) {
+	public static double MinValue(ChessBoard board, double alpha, double beta, int currentPly) {
 		// if terminal / cutoff reached
 		// return board.Utility();
 		if(cutoffTest(board))
 			return board.Utility();
 		
-		int v = Integer.MAX_VALUE;
+		currentPly++;
+		
+		double v = Double.MAX_VALUE;
 		
 		// for each move we can make in this state
-		//	v = Min(v, MaxValue(/*result of this move*/, alpha, beta)); 
+		//	v = Min(v, MaxValue(/*result of this move*/, alpha, beta, currentPly)); 
 		//	if(v < alpha)
 		//		return v;
 		//	alpha = Min(beta, v);
 			
+		Map<Integer, ArrayList<byte[]>> moves = board.getAllLegalMoves(board.getOppontentColor());
+		ChessBoard newBoard;
+		for(Integer startPos : moves.keySet()) {
+			byte startFile = (byte) (startPos % 10);
+			byte startRank = (byte) (startPos - startFile % 100);
+			for(byte[] endPos : moves.get(startPos)) {
+				newBoard = board.clone();
+				newBoard.move(startRank, startFile, endPos[0], endPos[1]);
+				v = Math.min(v, MaxValue(newBoard, alpha, beta, currentPly));
+				if(v <= alpha)
+					return v;
+				beta = Math.min(beta, v);
+			}
+		}		
 		return v;
 	}
 	
 	
-	private static int Max(int num1, int num2) {
+	private static double Max(double num1, double num2) {
 		return num1 > num2 ? num1 : num2;
 	}
 	
-	private static int Min(int num1, int num2) {
+	private static double Min(double num1, double num2) {
 		return num1 < num2 ? num1 : num2;
 	}
 }
